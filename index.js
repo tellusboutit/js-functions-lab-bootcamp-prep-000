af12038954e8243b70934dff9ea08a1ac2773cd6
Terrell Tullis <t.tullis34@outlook.com>
2017-08-13 14:58:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
 function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
